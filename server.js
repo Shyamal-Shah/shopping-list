4f76b578b34306578df7bbab6a97f4f74f96877f
Shyamal <shahshyamal15@yahoo.com>
2021-01-29 22:00:47 +0530

    Created mongoDB server (server.js added)

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to Mongo
+mongoose
+  .connect(db, 
+    { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected...'))
+  .catch((err) => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server Started on ${PORT}`));
