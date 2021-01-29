de5ee06ed7e38316a8190ad2ab1c39a8f53a0899
Shyamal <shahshyamal15@yahoo.com>
2021-01-30 01:09:12 +0530

    Created Item Schema and routes

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+const items = require('./routes/api/items');
+
 const app = express();
 
 // BodyParser Middleware
@@ -12,10 +14,12 @@ const db = require('./config/keys').mongoURI;
 
 // Connect to Mongo
 mongoose
-  .connect(db, 
-    { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected...'))
   .catch((err) => console.log(err));
+
+// Use Routes
+app.use('/api/items', items);
 
 const PORT = process.env.PORT || 5000;
 
